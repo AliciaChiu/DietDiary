4442bad3e58d9090fb745485661bf155cf8fbc97
Alicia Chiu <aliciachiu1030@gmail.com>
2020-12-20 17:06:49 +0800

    刻newRecordVC

--- DietDiary/DietDiary/NewRecordVC.swift
+++ DietDiary/DietDiary/NewRecordVC.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class NewRecordVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var dayView: UIView!
+    
+    @IBOutlet weak var timeView: UIView!
+    
+    
+    @IBOutlet weak var addView: UIView!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor(red: 255/255, green: 252/255, blue: 184/255, alpha: 1)
+        self.dayView.layer.cornerRadius = 10.0
+        self.timeView.layer.cornerRadius = 10.0
+        self.addView.layer.cornerRadius = 10.0
+        
     }
     
 
